9e19ccfb7577b0760166948ec75ba83b2d626365
Mike Smith <grimbough@gmail.com>
2021-07-26 17:35:06 +0200

    Added option to provide uniprot ID

--- R/reader.R
+++ R/reader.R
@@ -1,12 +1,48 @@
 
+.createURL <- function(input) {
+    url <- sprintf("https://alphafold.ebi.ac.uk/files/AF-%s-F1-predicted_aligned_error_v1.json",
+                   input)
+}
+
+#' @importFrom httr GET http_error content
+#' 
+.testURL <- function(url) {
+    res <- httr::GET(url) 
+    ## download error
+    if(http_error(res)) {
+        stop("Error downloading JSON file")
+    } 
+    return(content(res, type = "application/json"))
+}
+
+#' Read an AlphaFold predicted aligned error file
+#' 
+#' @param input Character vector of length 1.  Can be either a complete URL
+#' or local path to a predicted aligned error JSON file or a Uniprot protein 
+#' ID.
+#' 
+#' @returns An object of class [PredictedAlignedError-class]
+#' 
+#' @examples 
+#' ## using a Uniprot ID
+#' readPAE( "O82387" )
+#' 
+#' ## using the full URL to a JSON file
+#' readPAE( "https://alphafold.ebi.ac.uk/files/AF-O15552-F1-predicted_aligned_error_v1.json" )
+#' 
 #' @importFrom jsonlite fromJSON
 #' @importFrom methods new
 #' @export
-readPAE <- function(jsonFile) {
+readPAE <- function(input) {
     
-    json <- jsonFile |>
-        fromJSON() |> 
-        lapply(FUN = unlist)
+    is_url <- grepl("^http", x = input)
+    if(is_url) {
+        json <- fromJSON(input)
+    } else {
+        json <- .testURL(.createURL(input))[[1]]
+    }
+    
+    json <- lapply(json, FUN = unlist)
     
     new("PredictedAlignedError",
         residue1 = json$residue1,
